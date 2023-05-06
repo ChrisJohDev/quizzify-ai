f3ed748df962294aa5f3132035161f7f4391c59a
ChrisJohDev <chris@chrisjohannesson.com>
2023-05-06 10:53:33 +0200

    test

--- src/pages/queryResponse.tsx
+++ src/pages/queryResponse.tsx
@@ -1,8 +1,22 @@
-
-const QueryResponse = () => {
-  return(
-    <h1>Query Response</h1>
+const QueryResponse = ({ data }) => {
+  return (
+    <>
+      <h1>Query Response</h1>
+      <p>{data.name.toString()}</p>
+    </>
   );
-}
+};
+
+const getServerSideProps = async () => {
+  const res = await fetch("http://localhost:3000/api/hello");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
 
 export default QueryResponse;
+export { getServerSideProps };
