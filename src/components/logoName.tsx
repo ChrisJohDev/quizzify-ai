cf1e378af6b5d9a3865a8ef7f4b6895a19acdb37
ChrisJohDev <chris@chrisjohannesson.com>
2023-05-09 17:41:30 +0200

    Changed settings for logoName

--- src/components/logoName.tsx
+++ src/components/logoName.tsx
@@ -4,12 +4,14 @@ import Image, { ImageProps } from 'next/image';
 type LogoNameProps = ImageProps & {
   className?: string;
   width?: number;
-  height?: number;
+  height?: number | any;
 }
 
 const LogoName: React.FC<LogoNameProps> = ({className, width, height, ...res}) => {
+  const xheight = height || 'auto';
+  const xwidth = width || 160;
   return (
-    <Image src="/quizzify-name.svg" className={`${className}`} alt="Q-AI Logo" width={`${width || 160}`} height={`${height || 30}`} priority />
+    <Image src="/quizzify-name.svg" className={`${className}`} alt="Q-AI Logo" width={`${xwidth}`} height={`${xheight}`} priority />
   )
 }
 
