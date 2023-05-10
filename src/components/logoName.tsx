5b0eb903ccee88680daeccdda43c7c4fd48818ee
ChrisJohDev <chris@chrisjohannesson.com>
2023-05-10 12:31:17 +0200

    Fixed height problem for LogoName.tsx

--- src/components/logoName.tsx
+++ src/components/logoName.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Image, { ImageProps } from 'next/image';
+import styles from '@/styles/logoName.module.css'
 
 type LogoNameProps = ImageProps & {
   className?: string;
   width?: number;
-  height?: number | any;
+  height?: number;
 }
 
 const LogoName: React.FC<LogoNameProps> = ({className, width, height, ...res}) => {
-  const xheight = height || 'auto';
+  const xheight = height || 30;
   const xwidth = width || 160;
   return (
-    <Image src="/quizzify-name.svg" className={`${className}`} alt="Q-AI Logo" width={`${xwidth}`} height={`${xheight}`} priority />
+    <Image src="/quizzify-name.svg" className={`${className} ${styles.img}`} alt="Q-AI Logo" width={`${xwidth}`} height={`${xheight}`} priority />
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: React.FC = () => {
         </style>
       </Head>
       <div className="container max-w-screen-lg md:mx-auto flex flex-col home-page">
-        <LogoName className="mx-auto mt-8 mb-8" width={500} height={'auto'} src={''} alt={''} />
+        <LogoName className="mx-auto mt-8 mb-8" width={500} src={''} alt={''} />
         <p className={`text-center text-lg ${styles.home_p}`}>Quizzify-AI the number one stop for custom quizzes!</p>
         <Logo className="mainLogo mx-auto mt-8" width={500} src={''} alt={''} />
       </div>
