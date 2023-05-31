12174860e4bc9b84621404026e0d06aef938c6db
ChrisJohDev <chris@chrisjohannesson.com>
2023-05-31 15:26:55 +0200

    Updated link to pricing page. Added functionality and design to reset button in quizForm

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,31 +1,47 @@
 import styles from '@/styles/pricing.module.css'
 import Loading from '@/components/loading';
 
+const notImplemented = true;
+
 export default function Pricing() {
   return (
     <div className={`container flex flex-col mx-auto ${styles.pricingPage}`}>
-      <Loading text={"Loading this might be a ver y long text..."} />
-      <h1 className="text-center">Pricing Page</h1>
-      <div className={`mx-auto ${styles.tableWrapper}`}>
-        <table>
-          <thead>
-            <tr>
-              <th>Plan</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Basic</td>
-              <td>$10/month</td>
-            </tr>
-            <tr>
-              <td>Pro</td>
-              <td>25$/month</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      {
+        notImplemented
+          ? <div className={`${styles.tmp}`}>
+            <h1 className="text-center">Pricing not implemented!</h1>
+            <p>
+              Our pricing model is in the works and we will soon be able to provide Price information and different methods of payment.
+            </p>
+            <p>
+              Thank you for your patience and understanding.
+            </p>
+          </div>
+          :
+          <>
+            <Loading text={"Loading this might be a ver y long text..."} /><h1 className="text-center">Pricing Page</h1><div className={`mx-auto ${styles.tableWrapper}`}>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Plan</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Basic</td>
+                    <td>$10/month</td>
+                  </tr>
+                  <tr>
+                    <td>Pro</td>
+                    <td>25$/month</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </>
+      }
+
     </div>
 
   )
